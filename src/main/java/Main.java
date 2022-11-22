75c13f51ff3bfb6db765893a7870a2326b779e73
indup01 <ip620@ic.ac.uk>
2022-11-22 16:45:40 +0000

    v1.0-ud1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         int sum = 0;
-        System.out.println("Good Morning");
+        System.out.println("Good Morning Universe");
         for(int i = 1;i<= 10;i = i+1){
             System.out.println(i);
         }
